94fc1d0f909bd1ed5dcff51506d231355c5cdc1c
puppetagent <69377529+puppetagent@users.noreply.github.com>
2020-09-04 23:11:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,73 @@
+var helicopterIMG, helicopterSprite, packageSprite, packageIMG;
+var packageBody,ground
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-var engine, world;
-var box1;
-
-function setup(){
-    var canvas = createCanvas(400,400);
-    engine = Engine.create();
-    world = engine.world;
-
-    box1 = new Box(200,300,50,50);
-    box2 = new Box (240,100,50,100);
-    ground= new Ground (200,390,400,20);
-
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
 
-function draw(){
-    background(0);
-    Engine.update(engine);
-    box1.display();
-    box2.display();
-    ground.display();
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:true});
+	World.add(world, packageBody);
+	
+	rect1 = new Rect(width/2,640,250,10);
+	rect2 = new Rect(350,640,10,240);
+	rect3 = new Rect(450,640,10,240);
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
 }
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+	Engine.update(engine);
+	rect1.display();
+	rect2.display();
+	rect3.display();
+  keyPressed();
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+   Matter.Body.setStatic(packageBody,false) ;
+   
+  }
+}
+
+
+
